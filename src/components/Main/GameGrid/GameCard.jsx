9d0570cfaaae265ed949109c5451bc2d40188ede
Essmann <kenwilliam1000@gmail.com>
2025-10-28 15:23:48 +0100

    styling

--- src/components/Main/GameGrid/GameCard.jsx
+++ src/components/Main/GameGrid/GameCard.jsx
@@ -1,17 +1,18 @@
 function GameCard({ game }) {
   const t = URL.createObjectURL(new Blob([game.poster], { type: "image/png" }));
   return (
-    <div className="game_card ">
-      <img className="game_card_image" src={t || ""} alt={game.title} />
-      <GameFooter game={game} />
-    </div>
+    <>
+      <div className="game_card ">
+        <img className="game_card_image" src={t || ""} alt={game.title} />
+        <div className="title_overlay">{game.title}</div>
+        <GameFooter game={game} />
+      </div>
+    </>
   );
 }
 
 export default GameCard;
 
 function GameFooter({ game }) {
-  return <div className="game_footer">
-    
-  </div>;
+  return <div className="game_footer"></div>;
 }
