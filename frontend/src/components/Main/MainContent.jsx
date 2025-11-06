8ee818e91fbedc486b578f8a22a4697f81e5e9ae
Essmann <kenwilliam1000@gmail.com>
2025-11-06 10:38:10 +0100

    main content header changes

--- frontend/src/components/Main/MainContent.jsx
+++ frontend/src/components/Main/MainContent.jsx
@@ -1,21 +1,47 @@
 import GameGrid from "./GameGrid/GameGrid.jsx";
 import dummyGames from "../../test_data/dummyGames.jsx";
-import WindowIcon from '@mui/icons-material/Window';
-import ReorderIcon from '@mui/icons-material/Reorder';
+import WindowIcon from "@mui/icons-material/Window";
+import ReorderIcon from "@mui/icons-material/Reorder";
 import { useState } from "react";
-function MainContent({games, setGames, sidebarIndex}){
-    const [selectedHeaderItem, setSelectedHeaderItem] = useState(0);
-    return (
-        <div className="main_content">
-           <div className="main_content_header">
-                       <span className="list_view selected"><ReorderIcon fontSize="large"/></span>
-                       <span className=""><WindowIcon fontSize="large"/></span>
-                   </div>
-            {sidebarIndex == 0 && <GameGrid games={games} setGames={setGames}/>}    
-            {sidebarIndex == 1 && <GameGrid games={games.filter((game)=>game.favorite)} setGames={setGames}/>}     
-
-        </div>   
-    )
+import SearchIcon from '@mui/icons-material/Search';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+function MainContent({ games, setGames, sidebarIndex }) {
+  const [selectedHeaderItem, setSelectedHeaderItem] = useState(0);
+  return (
+    <div className="main_content">
+      <HeaderItem />
+      {sidebarIndex == 0 && <GameGrid games={games} setGames={setGames} />}
+      {sidebarIndex == 1 && (
+        <GameGrid
+          games={games.filter((game) => game.favorite)}
+          setGames={setGames}
+        />
+      )}
+    </div>
+  );
 }
 
 export default MainContent;
+
+function HeaderItem({}) {
+  return (
+    <div className="main_content_header">
+        <span className="header_search">
+            <SearchIcon fontSize="small"/>
+            <input type="text" placeholder="Search games..." />
+        </span>
+        <span className="header_filter">
+            <FilterAltIcon fontSize="small"/>
+            <button> Filter</button>
+        </span>
+      <div className="display_btns">
+        <span className="list_view selected">
+        <ReorderIcon fontSize="large" />
+      </span>
+      <span className="">
+        <WindowIcon fontSize="large" />
+      </span>
+      </div>
+    </div>
+  );
+}
