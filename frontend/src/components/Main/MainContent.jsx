f9ddc9ffb743f7f2c61c299dd62367e795ce707a
Essmann <kenwilliam1000@gmail.com>
2025-11-06 10:44:51 +0100

    header style changes

--- frontend/src/components/Main/MainContent.jsx
+++ frontend/src/components/Main/MainContent.jsx
@@ -5,6 +5,7 @@ import ReorderIcon from "@mui/icons-material/Reorder";
 import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { Slider } from "@mui/material";
 function MainContent({ games, setGames, sidebarIndex }) {
   const [selectedHeaderItem, setSelectedHeaderItem] = useState(0);
   return (
@@ -34,7 +35,11 @@ function HeaderItem({}) {
             <FilterAltIcon fontSize="small"/>
             <button> Filter</button>
         </span>
+        
       <div className="display_btns">
+        <div className="header_slider">
+            <Slider size="small" color=""/>
+        </div>
         <span className="list_view selected">
         <ReorderIcon fontSize="large" />
       </span>
