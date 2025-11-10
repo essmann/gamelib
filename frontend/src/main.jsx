e9ab1dcc675fe1cb2e06b9a2181e031db16f3e05
Essmann <kenwilliam1000@gmail.com>
2025-11-10 14:33:16 +0100

    wrapped App in the userprovider.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './styling/gameCard.css'
-import './styling/sidebar.css'
-import './styling/addGameMenu.css'
-import './styling/gameMenu.css'
-import './styling/searchMenu.css'
-import App from './App.jsx'
-import ContextProvider from './Context/ContextProvider.jsx' // ✅ correct import
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./styling/gameCard.css";
+import "./styling/sidebar.css";
+import "./styling/addGameMenu.css";
+import "./styling/gameMenu.css";
+import "./styling/searchMenu.css";
+import App from "./App.jsx";
+import ContextProvider from "./Context/ContextProvider.jsx"; // ✅ correct import
+import UserContextProvider from "./Context/UserContextProvider.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ContextProvider>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </ContextProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
